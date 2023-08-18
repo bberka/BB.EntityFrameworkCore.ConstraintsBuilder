f01872fe3a7698aa89c5a673e0387582d5b7471c
berkaybyr <berkaybyr@icloud.com>
2023-08-18 17:24:46 +0300

    missing files

---
+++ EfCore.ConstraintsBuilder.Sample/UserEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace EfCore.ConstraintsBuilder.Sample;
+
+public class UserEntity
+{
+  public string Name { get; set; }
+  public string Id { get; set; }
+  public string LastName { get; set; }
+  public int Age { get; set; }
+  public int AccountValidFor { get; set; }
+}
