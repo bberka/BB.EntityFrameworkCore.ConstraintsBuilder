ac1d0907a8e5b58a6f5d6399ec2a1ceffd73357e
berkaybyr <berkaybyr@icloud.com>
2023-08-18 17:40:40 +0300

    0.0.3 data annotations rules apply new methods for constrain regex

--- EfCore.ConstraintsBuilder/ConstrainsBuilderExtensions.cs
+++ EfCore.ConstraintsBuilder/ConstrainsBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq.Expressions;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -6,7 +7,6 @@ namespace EfCore.ConstraintsBuilder;
 
 public static class ConstrainsBuilderExtensions
 {
-  
   /// <summary>
   /// Adds constraint builder to current entity type builder
   /// </summary>
@@ -16,10 +16,84 @@ public static class ConstrainsBuilderExtensions
   /// <typeparam name="TEntity"></typeparam>
   /// <typeparam name="TProperty"></typeparam>
   /// <returns></returns>
-  public static ConstraintsBuilder<TEntity> AddConstraintsFor<TEntity,TProperty>(this EntityTypeBuilder<TEntity> builder, 
-                                                                                 Expression<Func<TEntity, TProperty>> keySelector, 
-                                                                                 SupportedConstraintServerType serverType = SupportedConstraintServerType.SqlServer) where TEntity : class
-  {
+  public static ConstraintsBuilder<TEntity> AddConstraintsFor<TEntity, TProperty>(this EntityTypeBuilder<TEntity> builder,
+                                                                                  Expression<Func<TEntity, TProperty>> keySelector,
+                                                                                  SupportedConstraintServerType serverType = SupportedConstraintServerType.SqlServer) where TEntity : class {
     return new ConstraintsBuilder<TEntity>(builder, keySelector.GetPropertyAccess().Name, serverType);
+  }
+
+ internal static ConstraintsBuilder<TEntity> AddConstraintsFor<TEntity>(this EntityTypeBuilder<TEntity> builder,
+                                                                                 string name,
+                                                                                 SupportedConstraintServerType serverType = SupportedConstraintServerType.SqlServer) where TEntity : class {
+   return new ConstraintsBuilder<TEntity>(builder, name, serverType);
+ }
+
+  public static void AddConstraintsFromDataAnnotations<TEntity>(this EntityTypeBuilder<TEntity> builder,
+                                                                SupportedConstraintServerType serverType = SupportedConstraintServerType.SqlServer) where TEntity : class {
+    var properties = typeof(TEntity).GetProperties();
+    var rangeAttribute = typeof(RangeAttribute);
+    var stringLengthAttribute = typeof(StringLengthAttribute);
+    var maxLengthAttribute = typeof(MaxLengthAttribute);
+    var minLengthAttribute = typeof(MinLengthAttribute);
+    var phoneAttribute = typeof(PhoneAttribute);
+    var emailAttribute = typeof(EmailAddressAttribute);
+    var creditCardAttribute = typeof(CreditCardAttribute);
+    var urlAttribute = typeof(UrlAttribute);
+    var regexAttribute = typeof(RegularExpressionAttribute);
+    foreach (var prop in properties) {
+      var range = prop.GetCustomAttributes(rangeAttribute, false).FirstOrDefault() as RangeAttribute;
+      var stringLength = prop.GetCustomAttributes(stringLengthAttribute, false).FirstOrDefault() as StringLengthAttribute;
+      var maxLength = prop.GetCustomAttributes(maxLengthAttribute, false).FirstOrDefault() as MaxLengthAttribute;
+      var minLength = prop.GetCustomAttributes(minLengthAttribute, false).FirstOrDefault() as MinLengthAttribute;
+      var phone = prop.GetCustomAttributes(phoneAttribute, false).FirstOrDefault() as PhoneAttribute;
+      var email = prop.GetCustomAttributes(emailAttribute, false).FirstOrDefault() as EmailAddressAttribute;
+      var creditCard = prop.GetCustomAttributes(creditCardAttribute, false).FirstOrDefault() as CreditCardAttribute;
+      var url = prop.GetCustomAttributes(urlAttribute, false).FirstOrDefault() as UrlAttribute;
+      var regex = prop.GetCustomAttributes(regexAttribute, false).FirstOrDefault() as RegularExpressionAttribute;
+      if (range != null) {
+        builder.AddConstraintsFor(prop.Name,serverType)
+               .NumberInBetween("CK_" + prop.Name + "_Range", range.Minimum, range.Maximum);
+      }
+      
+      if (stringLength != null) {
+        builder.AddConstraintsFor(prop.Name,serverType)
+               .StringLengthBetween("CK_" + prop.Name + "_StringLength", stringLength.MinimumLength, stringLength.MaximumLength);
+      }
+      
+      if (maxLength != null) {
+        builder.AddConstraintsFor(prop.Name,serverType)
+               .StringMaxLength("CK_" + prop.Name + "_MaxLength", maxLength.Length);
+      }
+      
+      if (minLength != null) {
+        builder.AddConstraintsFor(prop.Name,serverType)
+               .StringMinLength("CK_" + prop.Name + "_MinLength", minLength.Length);
+      }
+      
+      if (phone != null) {
+        builder.AddConstraintsFor(prop.Name,serverType)
+               .PhoneNumber("CK_" + prop.Name + "_PhoneNumber");
+      }
+      
+      if (email != null) {
+        builder.AddConstraintsFor(prop.Name,serverType)
+               .EmailAddress("CK_" + prop.Name + "_EmailAddress");
+      }
+      
+      if (creditCard != null) {
+        builder.AddConstraintsFor(prop.Name,serverType)
+               .CreditCard("CK_" + prop.Name + "_CreditCard");
+      }
+      
+      if (url != null) {
+        builder.AddConstraintsFor(prop.Name,serverType)
+               .StringUrl("CK_" + prop.Name + "_Url");
+      }
+      
+      if (regex != null) {
+        builder.AddConstraintsFor(prop.Name,serverType)
+               .RegexExpression("CK_" + prop.Name + "_Regex", regex.Pattern);
+      }
+    }
   }
 }
--- EfCore.ConstraintsBuilder/ConstraintsBuilder.cs
+++ EfCore.ConstraintsBuilder/ConstraintsBuilder.cs
@@ -29,6 +29,10 @@ public sealed class ConstraintsBuilder<TEntity> where TEntity : class
     _builder.ToTable(x => x.HasCheckConstraint(uniqueConstraintName, $"\"{_columnName}\" ~ '{regex}'"));
     return this;
   }
+  internal ConstraintsBuilder<TEntity> NumberInBetween(string uniqueConstraintName, object min, object max) {
+    _builder.ToTable(x => x.HasCheckConstraint(uniqueConstraintName, $"\"{_columnName}\" >= {min} AND \"{_columnName}\" <= {max}"));
+    return this;
+  }
   public ConstraintsBuilder<TEntity> NumberInBetween(string uniqueConstraintName, int min, int max) {
     _builder.ToTable(x => x.HasCheckConstraint(uniqueConstraintName, $"\"{_columnName}\" >= {min} AND \"{_columnName}\" <= {max}"));
     return this;
@@ -45,6 +49,14 @@ public sealed class ConstraintsBuilder<TEntity> where TEntity : class
     _builder.ToTable(x => x.HasCheckConstraint(uniqueConstraintName, $"LENGTH(\"{_columnName}\") >= {minLength}"));
     return this;
   }
+  public ConstraintsBuilder<TEntity> StringMaxLength(string uniqueConstraintName,int maxLength) {
+    _builder.ToTable(x => x.HasCheckConstraint(uniqueConstraintName, $"LENGTH(\"{_columnName}\") <= {maxLength}"));
+    return this;
+  }
+  public ConstraintsBuilder<TEntity> StringLengthBetween(string uniqueConstraintName,int minLength, int maxLength) {
+    _builder.ToTable(x => x.HasCheckConstraint(uniqueConstraintName, $"LENGTH(\"{_columnName}\") >= {minLength} AND LENGTH(\"{_columnName}\") <= {maxLength}"));
+    return this;
+  }
   
   public ConstraintsBuilder<TEntity> StringEqualOneOf(string uniqueConstraintName,params string[] acceptedValues) {
     var values = string.Join(',', acceptedValues);
@@ -58,7 +70,7 @@ public sealed class ConstraintsBuilder<TEntity> where TEntity : class
     return this;
   }
   
-  public ConstraintsBuilder<TEntity> StringEmailAddress(string uniqueConstraintName) {
+  public ConstraintsBuilder<TEntity> EmailAddress(string uniqueConstraintName) {
     const string EmailRegex =  "^[^@]+@[^@]+$";
     _builder.ToTable(x => x.HasCheckConstraint(uniqueConstraintName, $"\"{_columnName}\" ~ '{EmailRegex}'"));
     return this;
@@ -67,6 +79,18 @@ public sealed class ConstraintsBuilder<TEntity> where TEntity : class
   public ConstraintsBuilder<TEntity> StringUrl(string uniqueConstraintName) {
     const string UrlRegex =  @"^(http://|https://|ftp://)";
     _builder.ToTable(x => x.HasCheckConstraint(uniqueConstraintName, $"\"{_columnName}\" ~ '{UrlRegex}'"));
+    return this;
+  }
+  
+  public ConstraintsBuilder<TEntity> PhoneNumber(string uniqueConstraintName) {
+    const string PhoneNumberRegex =  @"^(\+?1)?[2-9]\d{2}[2-9](?!11)\d{6}$";
+    _builder.ToTable(x => x.HasCheckConstraint(uniqueConstraintName, $"\"{_columnName}\" ~ '{PhoneNumberRegex}'"));
+    return this;
+  }
+  
+  public ConstraintsBuilder<TEntity> CreditCard(string uniqueConstraintName) {
+    const string CreditCardRegex =  @"^(\d{4}[- ]){3}\d{4}|\d{16}$";
+    _builder.ToTable(x => x.HasCheckConstraint(uniqueConstraintName, $"\"{_columnName}\" ~ '{CreditCardRegex}'"));
     return this;
   }
 }
